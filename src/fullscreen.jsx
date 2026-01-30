b46bda3f89e5fa7dd4d1baf92488814267afe360
John Miller <johnmiller@macbookpro.attlocal.net>
2026-01-30 00:51:01 -0800

    commiting frontend

--- src/fullscreen.jsx
+++ src/fullscreen.jsx
@@ -112,7 +112,7 @@ function Fullscreen(props) {
                                 type: selectedTurbine, 
                                 long: document.getElementById("long").value, 
                                 lat:document.getElementById("lat").value,
-                                //date: officialDate,
+                                date: officialDate,
                                 valid:true
                                 }])  
                                 
